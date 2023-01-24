2710403e8c3a26afd494099d74d14d62029a2978
Musa <77996753+MusaAqeel@users.noreply.github.com>
2023-01-23 22:03:22 -0500

    Update Chat.js

--- HTV7-myai-final/react-client/src/components/Chat.js
+++ HTV7-myai-final/react-client/src/components/Chat.js
@@ -21,7 +21,7 @@ function Chat() {
     console.log("submit" + messageToSend);
     axios
       .post(
-        "https://my-ai-server-d9j7cxy1m-musaaqeel.vercel.app/",
+        "https://my-ai-server-sand.vercel.app/",
         { message: messageToSend },
         header
       )
